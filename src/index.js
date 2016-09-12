101d82dacad519a54018f823da56947ed3822cd1
Wolmir Nemitz <wolmir.nemitz@gmail.com>
2016-09-12 11:07:16 -0300

    Mudar gerenciador para Fonte

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
+var Fonte              = require('./fonte');
 var Classe             = require('pyoo.js');
+var Agente             = require('./agente');
 var Estado             = require('./estado');
 var Maquina            = require('./fsm');
 
 var gea = {};
 
+gea.Fonte              = Fonte;
+gea.Agente             = Agente;
 gea.Estado             = Estado;
 gea.Maquina            = Maquina;
 
